07ca1da7a8d7d2e0995e576ec0272d63b335da8e
Based <alexbroseph@mail.ru>
2022-02-13 14:41:04 +0400

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fbecc811-08cd-4ab4-9268-1f5be81056b6';
+export const MODAL_CLOSE_SEC = 2.5;
